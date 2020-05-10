10b6557037dfd4c13b638b40378af671a6b103ba
k-puneeth <puneeth.sarma@iiitb.org>
2020-05-10 23:53:06 +0530

    New Test

--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -11,43 +11,43 @@ public class CalculatorTests {
 
     @Test
     public void testAdd() {
-        int a = 15;
-        int b = 20;
-        int expectedResult = 35;
+        int a = 20;
+        int b = 30;
+        int expectedResult = 50;
         long result = calculator.add(a, b);
         Assert.assertEquals(expectedResult, result);;
     }
 
     @Test
     public void testSubtract() {
-        int a = 25;
+        int a = 35;
         int b = 20;
-        int expectedResult = 5;
+        int expectedResult = 15;
         long result = calculator.subtract(a, b);
         Assert.assertEquals(expectedResult, result);;
     }
 
     @Test
     public void testMultiply() {
-        int a = 10;
+        int a = 100;
         int b = 25;
-        long expectedResult = 250;
+        long expectedResult = 2500;
         long result = calculator.multiply(a, b);
         Assert.assertEquals(expectedResult, result);;
     }
 
     @Test
     public void testDivide() {
-        int a = 56;
+        int a = 560;
         int b = 10;
-        double expectedResult = 5.6;
+        double expectedResult = 56;
         double result = calculator.divide(a, b);
         Assert.assertEquals(expectedResult, result,0.00005);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testDivideByZero() {
-        int a = 15;
+        int a = 1500;
         int b = 0;
         calculator.divide(a, b);
     }
